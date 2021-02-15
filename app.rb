8b2d80b08c45cd1d58b6e026f3ab6f54d33aef48
Jung Lee <lee.jung.s95@gmail.com>
2021-02-15 19:55:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,25 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    @user_name.reverse
+  end
+
+  get '/square/:number' do
+    @user_number = params[:number].to_i
+    "#{@user_number ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    answer = ''
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+    answer
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    
+  end
 end
