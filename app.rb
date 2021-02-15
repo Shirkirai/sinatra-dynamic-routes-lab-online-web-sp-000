f6af49249466fcecf981a41029add3968c5719fa
Jung Lee <lee.jung.s95@gmail.com>
2021-02-15 20:59:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
       answer = ''
       params[:word1] do
-          answer = params[:word1] + params[:word2]
+          answer = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
       end
       answer
 
