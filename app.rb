c7735dd90305fb000cbdba7c65dc4f7ac699de54
Jung Lee <lee.jung.s95@gmail.com>
2021-02-15 22:04:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-      @operation = params[:operation].to_s
+      @operation = params[:operation]
 
       @number1 = params[:number1].to_i
       @number2 = params[:number2].to_i
